afa55abfa6f293333f9d095767798c16032a508e
ghxst0 <lfoxy18@gmail.com>
2022-12-14 23:16:55 +0100

    details page
    Co-authored-by: dudasdominik <dudasdominik@users.noreply.github.com>

--- frontend/details/details.js
+++ frontend/details/details.js
@@ -31,4 +31,30 @@ async function getShirtData() {
     fillContent(shirt);
 }
 
+document.querySelector("#order-minus").addEventListener("click", () => {
+    document.querySelector("#order-num").value = parseInt(document.querySelector("#order-num").value) - 1;
+    if (Number(document.querySelector("#order-num").value - 1) < 1) {
+        document.querySelector("#order-num").value = 1;
+        document.querySelector("#order-minus").setAttribute("disabled", "disabled");
+    } else {
+        document.querySelector("#order-plus").removeAttribute("disabled");
+    }
+})
+
+document.querySelector("#order-plus").addEventListener("click", async function(e) {
+    document.querySelector("#order-num").value = parseInt(document.querySelector("#order-num").value) + 1;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const response = await fetch(`/api/shirts/${urlParams.get("id")}`);
+    const shirt = await response.json();
+    if (Number(document.querySelector("#order-num").value) + 1 > shirt.stock) {
+        document.querySelector("#order-num").value = shirt.stock;
+        document.querySelector("#order-plus").setAttribute("disabled", "disabled");
+    } else {
+        document.querySelector("#order-minus").removeAttribute("disabled");
+    }
+})
+
+
 getShirtData();
+document.querySelector("#order-minus").setAttribute("disabled", "disabled");
